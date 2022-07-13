3d2dc4a2e013cf6f4cabe6969d6102ed7a668064
duxjava <duxjava@gmail.com>
2022-07-13 17:10:24 +0300

    test rpc

--- plugin.go
+++ plugin.go
@@ -4,7 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/roadrunner-server/api/v2/plugins/config"
+	"github.com/roadrunner-server/errors"
 	"go.uber.org/zap"
+	"time"
 )
 
 // plugin name
@@ -31,9 +34,38 @@ type Link struct {
 	GenUrl bool   `json:"gen_url"`
 }
 
-func (p *Plugin) Init(log *zap.Logger) error {
+func (p *Plugin) Init(cfg config.Configurer, log *zap.Logger) error {
 	p.clicks = make(chan string)
 	p.log = log
+
+	const op = errors.Op("my_plugin_init")
+	if !cfg.Has(name) {
+		return errors.E(errors.Disabled)
+	}
+
+	p.cfg = &Config{}
+	err := cfg.UnmarshalKey(name, p.cfg)
+
+	if err != nil {
+		return errors.E(op, err)
+	}
+
+	db, err := sql.Open("mysql", p.cfg.Mysql.Connection)
+	db.SetMaxIdleConns(p.cfg.Mysql.Maxidle)
+	db.SetMaxOpenConns(p.cfg.Mysql.Maxopen)
+	db.SetConnMaxLifetime(time.Second * p.cfg.Mysql.Lifetime)
+
+	if err != nil {
+		return errors.E(op, err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return errors.E(op, err)
+	}
+
+	p.db = db
 
 	return nil
 }
